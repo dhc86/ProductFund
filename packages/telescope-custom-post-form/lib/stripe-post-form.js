18b931957aadfca1379f4f03e11e1ca33ec0ea40
DIEGO <diegoh_c@yahoo.com>
2015-11-16 19:40:11 -0800

    working on stripe form posts

---
+++ packages/telescope-custom-post-form/lib/stripe-post-form.js
@@ -0,0 +1,50 @@
+
+// secret key:    sk_test_G77gaaVCcCaEFTccvZx04IFC  --> keep in server side
+// publishable key:   pk_test_GpmbjLyT5iOAfAPK7zT7DkF1  --> keep in client side
+
+// if (Meteor.isClient){
+//   // this code runs on the client
+// }
+
+// if (Meteor.isServer){
+//   // this code runs on the server
+// }
+
+ 
+if (Meteor.isClient) {
+  Template.custom_submit_button.events({
+    'click button': function(e) {
+      e.preventDefault();
+
+      StripeCheckout.open({
+        key: 'pk_test_GpmbjLyT5iOAfAPK7zT7DkF1',
+        amount: 1000, // this is equivalent to $10
+        name: 'Meteor Tutorial',
+        description: 'On how to use Stripe ($10.00)',
+        panelLabel: 'Pay Now',
+        token: function(res) {
+          stripeToken = res.id;
+          console.info(res);
+          Meteor.call('chargeCard', stripeToken);
+        }
+      });
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    'chargeCard': function(stripeToken) {
+      check(stripeToken, String);
+      var Stripe = StripeAPI('sk_test_G77gaaVCcCaEFTccvZx04IFC');
+
+      Stripe.charges.create({
+        source: stripeToken,
+        amount: 1000, // this is equivalent to $10
+        currency: 'usd'
+      }, function(err, charge) {
+        console.log(err, charge);
+      });
+    }
+  });
+}
--- packages/telescope-custom-post-form/package.js
+++ packages/telescope-custom-post-form/package.js
@@ -1,16 +1,21 @@
 Package.describe({
   summary: 'Telescope custom package',
-  version: '1.0.2',
+  version: '1.0.3',
   name: 'telescope-custom-post-form'
 });
 
 Package.onUse(function (api) {  
   api.use([
         'telescope:lib@0.25.5'
-    ])
+    ]);
 
   api.addFiles([
     'lib/client/templates/post_submit.html',
     'lib/client/templates/submit_button.html'],
     ['client']);
+
+  api.addFiles([
+    'lib/stripe-post-form.js'],
+    ['client','server']);
+
 });
