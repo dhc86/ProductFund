fe0b6aeecb65c0eae209b8b6d09d2f9b2b1d1973
Henry Yoo <henryhs.yoo@gmail.com>
2015-11-18 18:11:14 -0800

    Refactor posts sass file

--- packages/telescope-layout/lib/custom_modules.js
+++ packages/telescope-layout/lib/custom_modules.js
@@ -1,31 +1,3 @@
-// Telescope.modules.removeAll("postComponents");
-
-// Telescope.modules.add("postComponents", [
-//   {
-//     template: 'post_rank',
-//     order: 1
-//   },
-//   {
-//     template: 'post_vote',
-//     order: 10
-//   },
-//   {
-//     template: 'post_content',
-//     order: 20
-//   },
-//   {
-//     template: 'post_live',
-//     order: 30
-//   },
-//   {
-//     template: 'post_discuss',
-//     order: 40
-//   },
-//   {
-//     template: 'post_actions',
-//     order: 50
-//   }
-// ]);
 Telescope.modules.add("postComponents", {
   template: "post_live",
   order: 30
