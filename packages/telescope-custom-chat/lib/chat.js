d98b0735418c1eeb76202e1aba60fe8e01a751ae
Wilson Chu <wilsonjarrodchu@hotmail.com>
2015-11-21 02:00:41 +0000

    Variety of bug fixes. Channels and user list work properly

--- packages/telescope-custom-chat/lib/chat.js
+++ packages/telescope-custom-chat/lib/chat.js
@@ -1,63 +1,111 @@
 if (Meteor.isClient) {
     Meteor.subscribe("chat_messages");
     Meteor.subscribe("chat_rooms");
+    Meteor.subscribe("chat_users");
+    Session.setDefault("roomname", "General");
+
 
     Template.chat_messages.helpers({
         chat_messages: function() {
             // will later only return chatmessages for the current room
-            return chatMessages.find();
+            return chatMessages.find({room: Session.get("roomname")});
+        }
+    });
+
+    Template.chat_rooms.helpers({
+        chat_rooms: function() {
+            // will later only return chatrooms for the current channel
+            return chatRooms.find();
+        }
+    });
+
+    Template.chat_users.helpers({
+        chat_users: function() {
+            // will later only return users for the current room
+            return chatUsers.find({roomname: Session.get("roomname")}, {sort: {time: 1}});
         }
     });
 
     // These helpers only become available when document ready
     Template.chat_messages.rendered = function() {
+        var name = '';
+        if (Meteor.user()) name = Meteor.user().profile.name;
+        // randomized anon name
+        else { name = 'Anonymous' + (Math.floor(Math.random()*90000) + 10000).toString(); }
+        Session.set("name", name);
+
+        // detects duplicate names
+
         var scroll_down = function() {
             // autoscroll
             var chat = $('div#chat-room');
             // do not scroll if hovering on chat
+            // may potentially change to :focus
             var chatHover = chat.is(":hover");
             if (!chatHover) {
-                var height = 0;
-                chat.children().each(function(i, value){
-                    height += parseInt($(this).height());
-                });
-                height += '';
+                var height = chat.prop('scrollHeight');
                 chat.animate({scrollTop: height});
             }
             // clear input
-            document.getElementById('chat_message').value = '';
         }
-        Tracker.afterFlush(function() {
+
+        Meteor.setInterval(function() {
             scroll_down();
+        }, 500);
+
+        Tracker.afterFlush(function() {
             // timeout so page doesn't have to update all messages
             setTimeout(function() {
+                //initialize chat user for the user list
+                if (chatUsers._collection.update({name: name}, {name: name}) === 0) {
+                    var time = (new Date()).getTime();
+                    Meteor.call("newChatUser", {
+                        name: name,
+                        public_id: name,
+                        time: time,
+                        roomname: Session.get("roomname")});
+                }
+
                 Template.chat_messages.helpers({
                     update_scroll: scroll_down
                 });
-            }, 200);
+                var room_select = Session.get("roomname");
+                $('.room-select input[value=' + room_select + ']')
+                .parent()
+                .addClass('active');
+            }, 1);
         });
     };
 
     Template.chat_input.events = {
         'keydown input#chat_message' : function (event) {
             if (event.which == 13) { // 13 is the enter key
-                if (Meteor.user())
-                    var name = Meteor.user().profile.name;
-                else
-                    var name = 'Anonymous';
                 var chat_message = document.getElementById('chat_message')
                 if (chat_message.value != '') {
                     var chat_message_data = {
-                        name: name,
+                        name: Session.get("name"),
                         chat_message: chat_message.value,
-                        time: Date.now(),
+                        room: Session.get("roomname"),
+                        time: Date.now()
                     }
                     Meteor.call("newChatMessage", chat_message_data);
+                    document.getElementById('chat_message').value = '';
                     // chat_message.value = '';
                 }
             }
         }
     }
+
+    Template.chat_rooms.events = {
+        'click .room-select': function() {
+            Session.set("roomname", this.roomname);
+            Meteor.call("changeRoom", Session.get("name"), Session.get("roomname"));
+        }
+    }
+
+    Meteor.setInterval(function() {
+        Meteor.call('keepalive', Session.get("name"));
+    }, 5000);
 }
 
 if (Meteor.isServer) {
@@ -65,7 +113,10 @@ if (Meteor.isServer) {
     Meteor.startup(function () {
         chatMessages.remove({});
         chatRooms.remove({});
-        var chat_room_data = {roomname: "general"}
+        chatUsers.remove({});
+        var chat_room_data = {roomname: "General"}
+        Meteor.call("newChatRoom", chat_room_data);
+        chat_room_data = {roomname: "Random"}
         Meteor.call("newChatRoom", chat_room_data);
 
         Meteor.settings.public.extraEmoticons = [
@@ -95,6 +146,19 @@ if (Meteor.isServer) {
     Meteor.publish('chat_rooms', function() {
         return chatRooms.find({});
     });
+
+    Meteor.publish('chat_users', function() {
+        return chatUsers.find({});
+    });
+
+    // clears chat of outdated users
+    Meteor.setInterval(function() {
+        var now = (new Date()).getTime();
+        chatUsers.find({last_seen: {$lt: (now - 10000)}})
+        .forEach(function (user) {
+            chatUsers._collection.remove({_id: user._id});
+        });
+    }, 10000);
 }
 
 Meteor.methods({
@@ -103,5 +167,25 @@ Meteor.methods({
     },
     newChatRoom: function (chat_room_data) {
         chatRooms.insert(chat_room_data);
+    },
+    newChatUser: function (chat_user_data) {
+        chatUsers.insert(chat_user_data);
+    },
+    keepalive: function (name) {
+        chatUsers.update(
+            {public_id: name},
+            {$set: {last_seen: (new Date()).getTime()}}
+        );
+    },
+    changeRoom: function (name, roomname) {
+            var time = (new Date()).getTime();
+        chatUsers.update(
+            {name: name},
+            {$set: {roomname: roomname}}
+        );
+        chatUsers.update(
+            {name: name},
+            {$set: {time: time}}
+        );
     }
 });
--- packages/telescope-custom-chat/lib/chat_messages.js
+++ packages/telescope-custom-chat/lib/chat_messages.js
@@ -1,2 +1,3 @@
 chatMessages = new Mongo.Collection('chat_messages');
 chatRooms = new Mongo.Collection('chat_rooms');
+chatUsers = new Mongo.Collection('chat_users');
