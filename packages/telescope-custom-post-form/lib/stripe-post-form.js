95035fdd390c5e6ed8a1c5e2222ed83dc5ad8224
Holly-jolly <holly.haixin.l@gmail.com>
2015-11-21 16:12:08 -0800

    Merge conflicts solved

--- packages/telescope-custom-post-form/lib/stripe-post-form.js
+++ packages/telescope-custom-post-form/lib/stripe-post-form.js
@@ -6,63 +6,7 @@
   
 // href="https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_7NQ3TnZveKPdFms4tlzUx5wswjPDubGN&scope=read_write
 
-if(Meteor.isServer){
-
-    Meteor.methods({
-        fetchFromService: function(authentication_key) {
-            console.log("===========================================");
-            // console.log("authentication_key : ", authentication_key);
-
-            var url = "https://connect.stripe.com/oauth/token";
-
-            Meteor.http.post(url, {content:  
-                "client_secret=sk_test_G77gaaVCcCaEFTccvZx04IFC&"+
-                "code="+authentication_key+"&"+
-                "grant_type=authorization_code" }, 
-
-              function(error, result) {
-                console.log(result)
-                var access_token = result.data.access_token;
-                var stripe_publishable_key = result.data.stripe_publishable_key;
-                var stripe_user_id = result.data.stripe_user_id;
-                // console.log("access_token :",access_token);
-                //console.log("stripe_publishable_key :",stripe_publishable_key);
-                 // console.log("stripe_user_id :",stripe_user_id);
-
-
-                var current_user = Meteor.user();
-                var currentuserID= current_user._id
-               // console.log("currentuserID", currentuserID);
-
-                // current_user.req.params.Access_token = "diego herrera";
-               
-                // current_user.access_token = access_token;
-                
-                // PlayersList.insert({name: "David",score: 0});
-                // console.log('users', Users)
-                Users.update(currentuserID, {$set: {Stripe_user_id: stripe_user_id } });
-                // Users.update
-
-
-
-                // console.log("users ID ------------>: ", current_user._id);
-                // console.log("users public key ------------>: ", current_user.Public_key);
-                // console.log("users access_token ------------>: ", current_user.Access_token);
-
-                if(result.statusCode==200) {
-                    console.log("response received.");
-                }else if(result.statusCode===null){
-                  console.log("There response is null")
-                } 
-                else {
-                    console.log("Response issue: ", result.statusCode);
-                    // var errorJson = JSON.parse(result.content);
-                    // throw new Meteor.Error(result.statusCode, errorJson.error);
-                }
-            });
-        }
-    });
-
+if(Meteor.isServer){               
   Meteor.methods({
     fetchFromService: function(authentication_key) {
       console.log("===========================================");
@@ -100,12 +44,8 @@ if(Meteor.isServer){
       });
     }
   });
->>>>>>> 3103c86d00bf0255f7a0d6915a7cf2a316c7d0f3
+
 }
-
-
-
-
 
 
 if (Meteor.isClient) {
