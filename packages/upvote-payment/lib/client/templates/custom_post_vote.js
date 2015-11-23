7aac33800db9447dc1929454d38804cf5817a758
Holly-jolly <holly.haixin.l@gmail.com>
2015-11-22 21:23:14 -0800

    Once donation charge succeeds, donation detail and accumulated donation amount for that particular product is stored in database

--- packages/upvote-payment/lib/client/templates/custom_post_vote.js
+++ packages/upvote-payment/lib/client/templates/custom_post_vote.js
@@ -1,85 +1,25 @@
-if (Meteor.isClient)
-{
-  // Template.post_vote.helpers({
-  //   enableDownvotes: function () {
-  //     return Settings.get("enableDownvotes", false);
-  //   },
-  //   actionsClass: function () {
-  //     var user = Meteor.user();
-  //     var actionsClass = "";
-  //     if(!user) return false;
-  //     if (user.hasUpvoted(this)) {
-  //       actionsClass += " voted upvoted";
-  //     }
-  //     if (user.hasDownvoted(this)) {
-  //       actionsClass += " voted downvoted";
-  //     }
-  //     if (Settings.get("enableDownvotes", false)) {
-  //       actionsClass += " downvotes-enabled";
-  //     }
-  //     return actionsClass;
-  //   }
-  // });
-  Template.post_vote.events(
-  {
-    "click .upvote-link": function(e, t)
-    {
+if (Meteor.isClient){
+  Template.post_vote.events({
+    "click .upvote-link": function(e, t){
       var $donationModal = t.$('[data-modal=donationModal]');
       $donationModal.modal("show");
       e.preventDefault();
     }
   });
-
-  Template.post_vote.rendered = function()
-  {
+  Template.post_vote.rendered = function(){
     var post = this.data;
-    // var user = Meteor.user();
-    var $donationModal = this.$('[data-modal=donationModal]');
-    $donationModal.on('donation:success', function success(e, payload)
-    { 
-
-      console.log('Donated Successfully', e, payload);
-      //payload is an object storing whatever information stored in { amount: input_donation, stripe_transcation_id: "" }
-      // Meteor.call('upvotePost', post._id, function(){
-      //   Events.track("post upvoted", {'_id': post._id});
-      // })
-    });
+    var user = Meteor.user();
+    var $donationModal = this.$('[data-modal=donationModal]');  
   }
 }
 
   
-    // if(!user){
-    //   FlowRouter.go('signIn');
-    //   Messages.flash(i18n.t("please_log_in_first"), "info");
-    // } else if (user.hasUpvoted(post)) {
-    //   Meteor.call('cancelUpvotePost', post._id, function(){
-    //     Events.track("post upvote cancelled", {'_id': post._id});
-    //   });        
-    // } else {
-    //   Meteor.call('upvotePost', post._id, function(){
-    //     Events.track("post upvoted", {'_id': post._id});
-    //   });  
-    // }
-  // },
-  // 'click .downvote-link': function(e){
-  //   var post = this;
-  //   var user = Meteor.user();
-  //   e.preventDefault();
-  //   if(!user){
-  //     FlowRouter.go('atSignIn');
-  //     Messages.flash(i18n.t("please_log_in_first"), "info");
-  //   }
-  //   if (user.hasDownvoted(post)) {
-  //     Meteor.call('cancelDownvotePost', post._id, function(){
-  //       Events.track("post downvote cancelled", {'_id': post._id});
-  //     });        
-  //   } else {
-  //     Meteor.call('downvotePost', post._id, function(){
-  //       Events.track("post downvoted", {'_id': post._id});
-  //     });  
-  //   }
-  // }  
-// });
+// $donationModal.on('donation:success', function success(e, payload)
+    // { 
+
+    //   console.log('Donated Successfully', e, payload);
+    //   //payload is an object storing whatever information stored in { amount: input_donation, stripe_transcation_id: "" }
+    // });
 
 
 
--- packages/upvote-payment/lib/client/templates/donation_modal.js
+++ packages/upvote-payment/lib/client/templates/donation_modal.js
@@ -1,63 +1,28 @@
 if (Meteor.isClient) {
-  // Meteor.subscribe('owner-pk', stripe_pk);
-
   Template.donationModal.events({
     'click [data-action=confirm]': function(e, t){
-      // debugger 
       var $dom = t.$('[data-modal=donationModal]');
-      e.preventDefault();
-       
+      e.preventDefault(); 
       var input_donation = $dom.find('[data-content=donationAmount]').val();
-      
       var productName = this.title;
+      var productID= this._id;
       var productAuthorID= this.userId;
-
-      var stripe_account_info = Users.find({'_id': productAuthorID}).fetch();
-      var stripe_pk = stripe_account_info[0].Public_key
-
-      console.log ('stripe user id ' , stripe_account_info, "ower PK:", stripe_pk );
-       
       StripeCheckout.open({
-        key: 'pk_test_GpmbjLyT5iOAfAPK7zT7DkF1', // should be owner's PK.
+        key: 'pk_test_GpmbjLyT5iOAfAPK7zT7DkF1', // should be our platform's PK.
         amount: input_donation * 100, // this is equivalent to donationAmount
         name: 'Donation for' + ' ' + productName,
         description: 'Your total donation amount is $' + input_donation,
         panelLabel: 'Pay Now',
-        token: function(res) 
-        { stripeToken = res.id;
-          // Meteor.call('chargeTheCard', stripeToken, input_donation, productName, function(err, charge) {
-
-          //   // if charge then it worked
-          //   // else there was an error
-
-          // });
-           Meteor.call('chargeTheCard', stripeToken, input_donation, productName, productAuthorID );
-
-          // success
-          // lookup post by the post_id
-          // increase post.amount by amount
-          // save it
-          // Posts.update(upvotePostId, {$set: {Donations: input_donation} });
+        token: function(res) { 
+          stripeToken = res.id;
+          Meteor.call('chargeTheCard', stripeToken, input_donation, productName, productAuthorID, productID );
         }
       });
-
-      //authentication for charging the donator on behalf of the owner
-      //var stripe = require('stripe')('sk_test_zeEjXNMdvQ3wdADZ1PSCIcjd')(PLATFORM_SECRET_KEY);
-        // stripe.customers.create(
-        //   {description: "example@stripe.com"},
-        //   {stripe_account: CONNECTED_STRIPE_ACCOUNT_ID}
-        // );
-
-      $dom.trigger("donation:success", { amount: input_donation, stripe_transcation_id: "" });
-
       $dom.modal("hide");
     }
   });
 }
 
-//below code is used for saving donation amount into posts collection in database.
-//However, donation amount is not saved until stripe transaction suceeds
-// var upvotePostId = this._id
-// Posts.update(upvotePostId, {$set: {Donations: input_donation} });
+ //$dom.trigger("donation:success", { amount: input_donation, stripe_transcation_id: "" });
 
 
