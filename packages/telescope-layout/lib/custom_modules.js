69fb15dfe0b935e9ca13a0299169dacf8d624ebf
DIEGO <diegoh_c@yahoo.com>
2015-11-23 12:44:25 -0800

    ready to test layout

--- packages/telescope-layout/lib/custom_modules.js
+++ packages/telescope-layout/lib/custom_modules.js
@@ -3,10 +3,15 @@ Telescope.modules.add("postComponents", {
   order: 30
 });
 
-Telescope.modules.add("postHeading", {
-  template: "post_description",
-  order: 30
-})
+// Telescope.modules.add("postHeading", {
+//   template: "post_description",
+//   order: 30
+// });
+
+// Telescope.modules.add("totalDonation", {
+//   template: "total_donation",
+//   order: 10
+// });
 
 Telescope.modules.remove("postComponents", "post_avatars");
 Telescope.modules.remove("postComponents", "post_discuss");
