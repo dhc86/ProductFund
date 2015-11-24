2e8f51612c6cbdb06dbdd9ed1c5ffdd5ac7d4bc6
Holly-jolly <holly.haixin.l@gmail.com>
2015-11-24 11:03:44 -0800

    Fixed the total donation counter issue. Once the total donation is updated, the couter also updates

--- packages/upvote-payment/lib/client/templates/custom_post_vote.js
+++ packages/upvote-payment/lib/client/templates/custom_post_vote.js
@@ -9,6 +9,20 @@ if (Meteor.isClient){
   Template.post_vote.rendered = function(){
     var post = this.data;
     var user = Meteor.user();
-    var $donationModal = this.$('[data-modal=donationModal]');  
+    var $donationModal = this.$('[data-modal=donationModal]'); 
+
+    $donationModal.on("donation:success", function(error, payload){
+      $('.total-counter').each(function () {
+      $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+        }, {
+          duration: 4000,
+          easing: 'swing',
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          }
+        });   
+      });
+    });
   }
 }
