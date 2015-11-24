50e2121264a6ad847bffb0019c1b056900732707
DIEGO <diegoh_c@yahoo.com>
2015-11-23 16:57:02 -0800

    front page donation buttons working

--- packages/upvote-payment/lib/client/templates/custom_post_vote.js
+++ packages/upvote-payment/lib/client/templates/custom_post_vote.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient){
   Template.post_vote.events({
-    "click .upvote-link": function(e, t){
+    "click .donation-link": function(e, t){
       var $donationModal = t.$('[data-modal=donationModal]');
       $donationModal.modal("show");
       e.preventDefault();
@@ -12,20 +12,3 @@ if (Meteor.isClient){
     var $donationModal = this.$('[data-modal=donationModal]');  
   }
 }
-
-  
-// $donationModal.on('donation:success', function success(e, payload)
-    // { 
-
-    //   console.log('Donated Successfully', e, payload);
-    //   //payload is an object storing whatever information stored in { amount: input_donation, stripe_transcation_id: "" }
-    // });
-
-
-
-
-
-
-
-
-
